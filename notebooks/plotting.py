50c9f5f23fd4f950a9704052b013ded87358cb71
Diana Cai <caidcai@gmail.com>
2023-02-28 12:59:02 -0500

    cleaned up 2-simplex baseline synethtic example

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import jax.numpy as jnp 
 from active_search import get_next_y
+from gp_model import make_preds
 import ternary
 
 
@@ -77,4 +78,67 @@ def plot_triangle(pts, tight_pts, data, next_x = None, plot_design = True):
     tax.ticks(axis='lbr', linewidth=1, multiple=1, offset=0.02, fontsize=12)
     tax.show()
     
+    
+def plot_std(posterior, dataset, params, pts, next_x=None, scale=10, tight_pts=None, plot_design=False, plot_title=False, heatmapf=False):
+    
+    """
+    Scale: how finely to visualize the standard deviation.
+    """
+    
+    figure, ax = plt.subplots(figsize=(8, 5)); fontsize = 20
+    tax = ternary.TernaryAxesSubplot(ax=ax, scale=scale)
+
+    def get_std(x):  
+        test_x = jnp.array(x[0:2])
+        var = make_preds(dataset, posterior, params, jnp.atleast_2d(test_x))[1]
+        std = jnp.sqrt(var)
+        return float(std)
+
+    if plot_title:
+        tax.set_title("Posterior standard deviation and candidate design", fontsize=10)
+    tax.boundary(linewidth=2.0)
+    #tax.gridlines(multiple=n_grid, color="blue")
+
+    if tight_pts != None:
+        tax.scatter(jnp.array(tight_pts) * scale, marker="o", label="Tight points",
+                    sizes=jnp.ones(len(pts))*200, zorder=4, edgecolors="tab:purple", 
+                    linewidth=2.0, alpha=1, facecolors="none")
+        
+    # plot observed data
+    x_train_pts = []
+    for x in dataset.X:
+        x_train_pts.append((x[0] * scale, x[1] * scale, (1-x.sum()) * scale))
+    tax.scatter(x_train_pts, marker=".", label="Observation", s=250, zorder=5, color="k")
+
+    if plot_design:
+        tax.scatter(jnp.array(pts) * scale, marker=".", label="Design space",  s=150, color="gray", zorder=3)
+    
+    # plot standard deviation
+    stds = []
+    for i, p in enumerate(pts):
+        stds.append(get_std(p))
+        
+    if heatmapf:
+        tax.heatmapf(get_std, style="t", cmap=plt.cm.magma)
+    else:
+        tax.scatter(jnp.array(pts) * scale, cmap=plt.cm.magma, colormap=plt.cm.magma,
+            vmin=0, vmax=1, c=stds, colorbar=True, s=200, edgecolors="k")
+        
+        #print(pts[next_x][0:2], stds[next_x])
+    
+    # plot candidate design
+    if next_x != None:
+        #print(pts[next_x])
+        tax.scatter([jnp.array(pts[next_x]) * scale], marker="*", label="Candidate design", 
+                    s=200, zorder=6, color="tab:red", edgecolors="k")
+
+    
+    tax.clear_matplotlib_ticks()
+    tax.legend(loc="upper right")
+    tax.right_corner_label("B", fontsize=fontsize)
+    tax.top_corner_label("A", fontsize=fontsize)
+    tax.left_corner_label("C", fontsize=fontsize)
+    tax.get_axes().axis('off')
+
+    tax.show()
     
